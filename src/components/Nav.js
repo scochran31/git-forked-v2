3703f19ba3a4b02ef452e0f72718c703997dd662
Shawn Cochran <shawn.cochran31@gmail.com>
2021-08-31 18:27:10 -0600

    nav header

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import { ColorModeSwitcher } from '../ColorModeSwitcher'
-import { Box, Text } from '@chakra-ui/react'
+import { Box, Flex, Text } from '@chakra-ui/react'
 
 function Nav() {
     return (
-        <Box h={150} fontWeight='bold' fontStyle='italic' display='inline-flex' justify='space-between'>
-            <Text fontWeight='bold' fontStyle='italic'>Git Forked</Text>
-            <ColorModeSwitcher />
-        </Box>
+        <Flex
+            h={150}
+            fontWeight='bold'
+            fontStyle='italic'
+            fontSize='36px'
+            justify='space-between'
+            p={5}
+        >
+            <Box>
+                <Text fontWeight='bold' fontStyle='italic'>Git Forked</Text>
+            </Box>
+            <Box justifySelf='flex-end'>
+                <ColorModeSwitcher />
+            </Box>
+        </Flex>
     )
 }
 
